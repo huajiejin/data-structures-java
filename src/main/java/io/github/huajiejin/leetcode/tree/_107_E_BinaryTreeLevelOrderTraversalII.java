ff9cf0d700eadf1b35d11690899a351093c73a17
huajiejin <huajie920@gmail.com>
2020-11-17 11:52:42 +0800

    feat: add TreeNode util

---
+++ src/main/java/io/github/huajiejin/leetcode/tree/_107_E_BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,16 @@
+package io.github.huajiejin.leetcode.tree;
+
+import io.github.huajiejin.leetcode.util.TreeNode;
+
+import java.util.List;
+
+public class _107_E_BinaryTreeLevelOrderTraversalII {
+    public static void main(String[] args) {
+        TreeNode c1 = TreeNode.buildFrom(new Integer[]{3,9,20,null,null,15,7});
+        System.out.println(c1);
+    }
+
+    public List<List<Integer>> s1(TreeNode root) {
+        return null;
+    }
+}
---
+++ src/main/java/io/github/huajiejin/leetcode/util/TreeNode.java
@@ -0,0 +1,56 @@
+package io.github.huajiejin.leetcode.util;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class TreeNode {
+    Integer val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(Integer val) { this.val = val; }
+    TreeNode(Integer val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        List<Integer> l = new LinkedList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(this);
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            if (node != null) {
+                l.add(node.val);
+                q.offer(node.left);
+                q.offer(node.right);
+            } else {
+                l.add(null);
+            }
+        }
+        return l.toString();
+    }
+
+    public static TreeNode buildFrom(Integer[] elements) {
+        if (elements.length <= 0) return null;
+        TreeNode node = new TreeNode(elements[0]);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(node);
+        for (int i=1; i<elements.length; i+=2) {
+            TreeNode curNode = q.poll();
+            curNode.left = new TreeNode(elements[i]);
+            q.offer(curNode.left);
+            if (i+1 < elements.length) {
+                curNode.right = new TreeNode(elements[i+1]);
+                q.offer(curNode.right);
+            }
+        }
+        return node;
+    }
+}
